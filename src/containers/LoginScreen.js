6559eb0afd7b63051e6ec09e1419b921e3755f0e
kasthuri <sckl2000151@segi4u.my>
2022-11-22 10:16:57 +0700

    latest

--- src/containers/LoginScreen.js
+++ src/containers/LoginScreen.js
@@ -13,6 +13,7 @@ const LoginScreen = () => {
 
   const navigation = useNavigation();
 
+  /////// Gender Picker /////
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([
@@ -20,8 +21,10 @@ const LoginScreen = () => {
     {label: 'Female', value: 'Female'}
   ]);
 
+  /////////// Date Picker //////////
   const [selectedDate, setSelectedDate] = useState("");
 
+  /////////// Age Auto Fill Function //////////
   const [age, setAge] = useState("")
 
   function handleDateChange(date) {setSelectedDate(date);
@@ -33,7 +36,7 @@ const LoginScreen = () => {
     setAge(age)
   }
 
-
+  /////////// Login Function //////////
   const [signInEmail, setSignInEmail] = useState("");
   const [signInPassword, setSignInPassword] = useState("");
   const [errMessage, setErrMessage] = useState("");
@@ -55,24 +58,26 @@ const LoginScreen = () => {
         });
   }
 
-const [isSignUp, setIsSignUp] = useState (false);
-const [signUpEmail, setSignUpEmail] = useState('');
-const [signUpPassword, setSignUpPassword] = useState('');
+  /////////// Sign up Function //////////
+  const [isSignUp, setIsSignUp] = useState (false);
+  const [signUpEmail, setSignUpEmail] = useState('');
+  const [signUpPassword, setSignUpPassword] = useState('');
+  const [signUpConfirmPassword, setSignUpConfirmPassword] = useState('');
 
-function SignUp() {
-  createUserWithEmailAndPassword(auth, signUpEmail, signUpPassword)
-  .then((userCredential) => {
-    console.log('Signed Up')
-  })
-  .catch((error) => {
-    const err = error.message;
-    const one = err.replace('Firebase: Error', '')
-    const two = one.replace('(auth/', '')
-    const three = two.replace(').', '')
-    const newErr = three.replace('-', ' ')
-    setRegisterErrorMessage(newErr)
-  });
-}
+  function SignUp() {
+    createUserWithEmailAndPassword(auth, signUpEmail, signUpPassword,signUpConfirmPassword)
+    .then((userCredential) => {
+      console.log('Signed Up')
+    })
+    .catch((error) => {
+      const err = error.message;
+      const one = err.replace('Firebase: Error', '')
+      const two = one.replace('(auth/', '')
+      const three = two.replace(').', '')
+      const newErr = three.replace('-', ' ')
+      setRegisterErrorMessage(newErr)
+    });
+  }
 
 
 
@@ -82,23 +87,27 @@ function SignUp() {
       <ImageBackground style={styles.imageContainer} source={require('../assets/SigninBackground.jpg')}>
         { isSignUp === false ?
         <View style={styles.subContainer}>
-        <View style={{padding: 0}}>
+          <View style={{padding: 0}}>
             <Text style={{paddingVertical: 5, fontSize: 20, fontWeight:'800' , color:'#401F02'}}>Roomarch</Text>
           </View> 
+
           <View style={{padding: 0}}>
             <Text style={{paddingVertical: 5}}>Email:</Text>
             <TextInput style={styles.textInput} onChangeText={setSignInEmail}></TextInput>
           </View>
+
           <View style={{padding: 0}}>
             <Text style={{paddingVertical: 5}}>Password:</Text>
             <TextInput style={styles.textInput} secureTextEntry={true} onChangeText={setSignInPassword}></TextInput>
           </View>
           {errMessage !== "" && <Text>{errMessage}</Text>}
+
           <View style={{padding: 0}}>
             <TouchableOpacity style={styles.signInButton} onPress={()=> SignIn() } >
               <Text style={{color:'#FFFFFF'}}>Sign In</Text>
             </TouchableOpacity>
           </View>
+
           <View style={{padding: 0, flexDirection: 'row'}}>
               <Text style={{color:'#000000'}}>New User?</Text>
               <TouchableOpacity style={{}} onPress={() => {
@@ -107,8 +116,9 @@ function SignUp() {
               <Text style={{paddingLeft: 5, color:'#0A1172'}}>Sign Up</Text>
             </TouchableOpacity>
           </View>
+
           <View style={{padding: 0}}>
-              <TouchableOpacity style={{}}>
+            <TouchableOpacity style={{}}>
               <Text style={{ color:'#0A1172'}}>Forget Password?</Text>
             </TouchableOpacity>
           </View>
@@ -120,20 +130,25 @@ function SignUp() {
 ////// sign up page //////
         <View style={styles.signUpContainer}>
           <View style={{height: '10%', width: '100%', flexDirection: 'row', alignItems: 'center'}}>
+            
             <View style={{width: '40%'}}>
               <TouchableOpacity onPress={() => { setIsSignUp(false)}}>
               <AntDesignIcon name={'arrowleft'} size={25} style={{paddingLeft: 5}}/>
               </TouchableOpacity>
             </View>
+
             <View style={{alignItems: 'center', justifyContent: 'center'}}>
               <Text style={{fontWeight:'bold', fontSize:'20px'}}>Sign Up</Text>
             </View>
+
           </View>
           <View style={{height: '90%', alignItems: 'center', justifyContent: 'space-evenly'}}>
+            
             <View style={{padding: 0}}>
               <Text style={{paddingVertical: 5}}>Name:</Text>
               <TextInput style={styles.textInput}></TextInput>
             </View>
+
             <View style={{zIndex: 1000}}>
               <Text style={{paddingVertical: 5}}>Date Of Birth:</Text>
               <DatePicker style={styles.textInputDob}
@@ -145,7 +160,9 @@ function SignUp() {
                           showMonthDropdown
                           />
             </View>
+
             <View style={{flexDirection: 'row', gap: 10, zIndex: 999}}>
+                
                 <View style={{padding: 0}}>
                   <Text style={{paddingVertical: 5}}>Age:</Text>
                   <Text style={styles.textInputAge}>{age ? age : 0}</Text>
@@ -163,32 +180,38 @@ function SignUp() {
                                   />
                 </View>
             </View>
+
             <View style={{padding: 0}}>
               <Text style={{paddingVertical: 5}}>Email:</Text>
               <TextInput style={styles.textInput} keyboardType='email-address' onChangeText={setSignUpEmail}></TextInput>
             </View>
+
             <View style={{padding: 0}}>
               <Text style={{paddingVertical: 5}}>Phone No:</Text>
               <TextInput style={styles.textInput} keyboardType='number-pad' textContentType='telephoneNumber'></TextInput>
             </View>
+
             <View style={{padding: 0}}>
               <Text style={{paddingVertical: 5}}>Whatsapp No:</Text>
               <TextInput style={styles.textInput} keyboardType='number-pad' textContentType='telephoneNumber'></TextInput>
             </View>
+
             <View style={{padding: 0}}>
               <Text style={{paddingVertical: 5}}>Password:</Text>
               <TextInput style={styles.textInput} secureTextEntry={true} onChangeText={setSignUpPassword}></TextInput>
             </View>
+
             <View style={{padding: 0}}>
               <Text style={{paddingVertical: 5}}>Confirm Password:</Text>
-              <TextInput style={styles.textInput} secureTextEntry={true}></TextInput>
+              <TextInput style={styles.textInput} secureTextEntry={true} onChangeText={setSignUpConfirmPassword}></TextInput>
             </View>
+
             <View style={{padding: 15}}>
               <TouchableOpacity style={styles.signUpButton} onPress={() => { SignUp() }}>
                 <Text style={{color:'#FFFFFF'}}>Sign Up</Text>
               </TouchableOpacity>
               {registerErrorMessage !== "" && <Text>{registerErrorMessage}</Text>}
-          </View>
+            </View>
           </View>
         </View>
         }
@@ -197,16 +220,20 @@ function SignUp() {
   )
 }
 
+
+////// CSS ///////
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     
   },
+
   imageContainer: {
     flex: 1,
     alignItems: "center",
     justifyContent: 'center'
   },
+
   subContainer: {
     width: '50%',
     height: '40%',
@@ -260,7 +287,7 @@ const styles = StyleSheet.create({
 
   signUpContainer: {
     width: '70%',
-    height: '90%',
+    height: '95%',
     alignItems: "center",
     justifyContent: 'space-evenly',
     backgroundColor: '#FFEDDD',
@@ -292,7 +319,8 @@ const styles = StyleSheet.create({
     paddingLeft: 5,
     backgroundColor: 'white',
     height: 21,
-    width: 90
+    width: 90,
+    
   },
 
   textInputDob:{
